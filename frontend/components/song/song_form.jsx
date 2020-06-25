69aecb6f0f0d472d6dc0405b61a4c4b0cb92883b
Marquis Bentley <livefreekai@icloud.com>
2020-06-25 09:39:07 -0700

    Added a form component for creating a song object

--- frontend/components/song/song_form.jsx
+++ frontend/components/song/song_form.jsx
@@ -3,12 +3,35 @@ import React from 'react';
 class SongForm extends React.Component {
     constructor(props) {
         super(props)
+        this.state = { title: '', description: '' }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(field) {
+        return e => this.setState({ [field]: e.currentTarget.value })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.createSong(this.state);
     }
 
     render() {
         return (
             <div>
-
+                <form onSubmit={this.handleSubmit}>
+                    <label>Title: 
+                        <input type="text" placeholder='Enter a title'
+                        value={this.state.title} onChange={this.handleInput('title')}/>
+                    </label>
+                    <br/>
+                    <label>Description: 
+                        <input type="text" placeholder='Enter a description'
+                        value={this.state.description} onChange={this.handleInput('description')}/>
+                    </label>
+                    <br/>
+                    <input type="submit" value='create'/>
+                </form>
             </div>
         )
     }
