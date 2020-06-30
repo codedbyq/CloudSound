202ce3e80fa2379ba1df6bffb51616e1180b5211
Marquis Bentley <livefreekai@icloud.com>
2020-06-30 12:46:08 -0700

    Worked on the container component for the audio player

---
+++ frontend/components/audio_player/audio_player.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class AudioPlayer extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className='audio-player-div'>
+
+            </div>
+        )
+    }
+}
+
+export default AudioPlayer;
--- frontend/components/audio_player/audio_player_container.js
+++ frontend/components/audio_player/audio_player_container.js
@@ -1,1 +1,15 @@
 import { connect } from 'react-redux';
+import audioPlayer from './audio_player';
+import { togglePlay, receiveCurrentSong } from '../../actions/audio_actions';
+
+const mapStateToProps = (state) => ({
+    playing: state.ui.audioPlayer.playing,
+    currentSong: state.ui.musicPlayer.currentSong
+});
+
+const mapDispatchToProps = dispatch => ({
+    receiveCurrentSong: song => dispatch(receiveCurrentSong(song)),
+    togglePlay: () => dispatch(togglePlay())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(audioPlayer);
