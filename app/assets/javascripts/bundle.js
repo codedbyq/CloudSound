242c069e51a9d574c65c845f63da8d4aac086776
Marquis Bentley <livefreekai@icloud.com>
2020-08-08 17:51:46 -0700

    Refactor the navbar component to use react hooks

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1601,126 +1601,90 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
 /* harmony import */ var _nav_dropdown__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./nav_dropdown */ "./frontend/components/navbar/nav_dropdown.jsx");
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 
 
 
-var NavBar = /*#__PURE__*/function (_React$Component) {
-  _inherits(NavBar, _React$Component);
+var NavBar = function NavBar(props) {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(''),
+      _useState2 = _slicedToArray(_useState, 2),
+      search = _useState2[0],
+      setSearch = _useState2[1];
 
-  var _super = _createSuper(NavBar);
+  var handleInput = function handleInput(e) {
+    setSearch(e.target.value);
+  };
 
-  function NavBar(props) {
-    var _this;
+  var handleSubmit = function handleSubmit(e) {
+    e.preventDefault();
+    props.search(search).then(function (res) {
+      return props.history.push("/search/".concat(state.search));
+    });
+  };
 
-    _classCallCheck(this, NavBar);
-
-    _this = _super.call(this, props);
-    _this.state = {
-      search: ''
-    };
-    _this.handleInput = _this.handleInput.bind(_assertThisInitialized(_this));
-    _this.handleSubmit = _this.handleSubmit.bind(_assertThisInitialized(_this));
-    return _this;
-  }
-
-  _createClass(NavBar, [{
-    key: "handleInput",
-    value: function handleInput(e) {
-      this.setState({
-        search: e.target.value
-      });
-    }
-  }, {
-    key: "handleSubmit",
-    value: function handleSubmit(e) {
-      var _this2 = this;
-
-      e.preventDefault();
-      this.props.search(this.state.search).then(function (res) {
-        return _this2.props.history.push("/search/".concat(_this2.state.search));
-      });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var placeholderMsg = this.props.currentUser ? 'Search for artists, bands, tracks, podcasts...' : 'Search for artists, tracks...';
-      var searchBar = this.props.currentUser ? 'search-long' : 'search';
-      var navRight = this.props.currentUser ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
-        id: "right-btn",
-        to: "/discover"
-      }, "Try Pro"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
-        id: "profile-drop",
-        to: "/users/".concat(this.props.currentUser)
-      }, "Profile")) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
-        className: "nav-session",
-        id: "login",
-        to: "/login"
-      }, "Sign in"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
-        className: "nav-session",
-        id: "signup",
-        to: "/signup"
-      }, "Create account"));
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        id: "navbar"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "nav-left"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
-        id: "nav-logo",
-        to: "/"
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], _defineProperty({
-        className: "left-btn",
-        to: "/discover"
-      }, "className", "home"), "Home"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
-        className: "left-btn",
-        href: "https://github.com/codedbyq/CloudSound"
-      }, "Github"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
-        className: "left-btn",
-        href: "https://www.linkedin.com/in/marquisbentley/"
-      }, "LinkedIn")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("form", {
-        onSubmit: this.handleSubmit
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-        id: searchBar,
-        type: "search",
-        placeholder: placeholderMsg,
-        onChange: this.handleInput
-      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        id: "nav-right"
-      }, navRight, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
-        id: "right-btn",
-        to: "/upload"
-      }, "Upload"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_nav_dropdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
-        logout: this.props.logout
-      })));
-    }
-  }]);
-
-  return NavBar;
-}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+  var placeholderMsg = props.currentUser ? "Search for artists, bands, tracks, podcasts..." : "Search for artists, tracks...";
+  var searchBar = props.currentUser ? "search-long" : "search";
+  var navRight = props.currentUser ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
+    id: "right-btn",
+    to: "/discover"
+  }, "Try Pro"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
+    id: "profile-drop",
+    to: "/users/".concat(props.currentUser)
+  }, "Profile")) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
+    className: "nav-session",
+    id: "login",
+    to: "/login"
+  }, "Sign in"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
+    className: "nav-session",
+    id: "signup",
+    to: "/signup"
+  }, "Create account"));
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    id: "navbar"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "nav-left"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
+    id: "nav-logo",
+    to: "/"
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], _defineProperty({
+    className: "left-btn",
+    to: "/discover"
+  }, "className", "home"), "Home"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    className: "left-btn",
+    href: "https://github.com/codedbyq/CloudSound"
+  }, "Github"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    className: "left-btn",
+    href: "https://www.linkedin.com/in/marquisbentley/"
+  }, "LinkedIn")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("form", {
+    onSubmit: handleSubmit
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    id: searchBar,
+    type: "search",
+    placeholder: placeholderMsg,
+    onChange: handleInput
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    id: "nav-right"
+  }, navRight, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["NavLink"], {
+    id: "right-btn",
+    to: "/upload"
+  }, "Upload"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_nav_dropdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    logout: props.logout
+  })));
+};
 
 /* harmony default export */ __webpack_exports__["default"] = (NavBar);
 
--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,62 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom'
 import NavDropdown from './nav_dropdown';
 
-class NavBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { search: '' };
-        this.handleInput = this.handleInput.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+const NavBar = (props) => {
+    const [search, setSearch] = useState('');
+
+    const handleInput = (e) => {
+        setSearch( e.target.value );
     }
 
-    handleInput(e) {
-        this.setState({ search: e.target.value });
-    }
-
-    handleSubmit(e) {
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        this.props.search(this.state.search)
-            .then(res => this.props.history.push(`/search/${this.state.search}`));
+        props.search(search)
+            .then(res => props.history.push(`/search/${state.search}`));
     }
 
-    render() {
-        const placeholderMsg = this.props.currentUser ? 'Search for artists, bands, tracks, podcasts...' : 'Search for artists, tracks...';
-        const searchBar = this.props.currentUser ? 'search-long' : 'search';
-        const navRight = this.props.currentUser ? (
-            <>
-                <NavLink id='right-btn' to='/discover'>Try Pro</NavLink>
-                <NavLink id='profile-drop' to={`/users/${this.props.currentUser}`}>Profile</NavLink>
-            </>
-        ) : (
-            <>
-                <NavLink className='nav-session' id='login' to='/login'>Sign in</NavLink>
-                <NavLink className='nav-session' id='signup' to='/signup'>Create account</NavLink>
-            </>
-        )
-    
-        return (
-            <div id='navbar'>
-                <div className='nav-left'>
-                    <NavLink id='nav-logo' to='/'></NavLink>
-                    <NavLink className='left-btn' to='/discover' className='home'>Home</NavLink>
-                    <a className='left-btn' href="https://github.com/codedbyq/CloudSound">Github</a>
-                    <a className='left-btn' href="https://www.linkedin.com/in/marquisbentley/">LinkedIn</a>
-                </div>
-    
-                <form onSubmit={this.handleSubmit}>
-                    <input id={searchBar} type="search" placeholder={placeholderMsg} onChange={this.handleInput}/>
-                </form>
-    
-                <div id='nav-right'>
-                    {navRight}
-                    <NavLink id='right-btn' to='/upload'>Upload</NavLink>
-                    <NavDropdown logout={this.props.logout} />
-                </div>
+    const placeholderMsg = props.currentUser
+      ? "Search for artists, bands, tracks, podcasts..."
+      : "Search for artists, tracks...";
+    const searchBar = props.currentUser ? "search-long" : "search";
+    const navRight = props.currentUser ? (
+        <>
+            <NavLink id="right-btn" to="/discover">Try Pro</NavLink>
+            <NavLink id="profile-drop" to={`/users/${props.currentUser}`}>Profile</NavLink>
+        </>
+    ) : (
+        <>
+            <NavLink className="nav-session" id="login" to="/login">Sign in</NavLink>
+            <NavLink className="nav-session" id="signup" to="/signup">Create account</NavLink>
+        </>
+    );
+
+    return (
+        <div id="navbar">
+            <div className="nav-left">
+                <NavLink id="nav-logo" to="/"></NavLink>
+                <NavLink className="left-btn" to="/discover" className="home">Home</NavLink>
+
+                <a className="left-btn" href="https://github.com/codedbyq/CloudSound">Github</a>
+                <a className="left-btn" href="https://www.linkedin.com/in/marquisbentley/">LinkedIn</a>
             </div>
-        )
-    }
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    id={searchBar}
+                    type="search"
+                    placeholder={placeholderMsg}
+                    onChange={handleInput}
+                />
+            </form>
+
+            <div id="nav-right">
+                {navRight}
+                <NavLink id="right-btn" to="/upload">Upload</NavLink>
+                <NavDropdown logout={props.logout} />
+            </div>
+      </div>
+    );
 }
 
 
